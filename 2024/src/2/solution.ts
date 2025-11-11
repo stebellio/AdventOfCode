8cac2191430b96d253643e229deb12182044ea3b
Stefano Bellio <stefano.bellioo@gmail.com>
2025-11-11 08:31:09 +0100

    2024 - Day2

--- 2024/src/2/solution.ts
+++ 2024/src/2/solution.ts
@@ -12,6 +12,29 @@ const checkIsSafe = (data: number[], callback: Function) => {
     return true;
 }
 
+
+const checkIsSafeRecursively = (data: number[], callback: Function) => {
+    for (let i = 0; i < data.length; i++) {
+        const j = i + 1;
+
+        if (j >= data.length) break;
+
+        const diff = Math.abs(data[i] - data[j]);
+        if (callback(data[i], data[j]) || diff < 1 || diff > 3) {
+            const removeFirst = [...data];
+            removeFirst.splice(0, 1);
+            const isSafeRemoveFirst = checkIsSafe(removeFirst, callback);
+
+            const removeJData = [...data];
+            removeJData.splice(j, 1);
+            const isSafeRemoveJ = checkIsSafe(removeJData, callback);
+
+            return isSafeRemoveFirst || isSafeRemoveJ;
+        }
+    }
+    return true;
+}
+
 console.log('Challenge 2');
 console.log('===================');
 
@@ -36,5 +59,19 @@ function firstPart () {
 }
 
 function secondPart () {
-    
+    const data = readFileRows();
+    let safeCount = 0;
+
+    for (const row of data) {
+        const elements = row.split(' ').map(Number);
+
+        if (
+            checkIsSafeRecursively(elements, (current: number, next: number) => current < next) ||
+            checkIsSafeRecursively(elements, (current: number, next: number) => current > next)
+        ) {
+            safeCount++;
+        }
+    }
+
+    console.log('Solution: ', safeCount);
 }
